4811e6f78935d4664b088b23b9702602df2a83b8
Rodrigo Gama <rodrigo1902.rb@gmail.com>
2023-05-09 19:29:34 -0300

    Implemented colored text, and better results

--- addon/operator/run_tests.py
+++ addon/operator/run_tests.py
@@ -3,17 +3,23 @@ import bpy
 import os
 import subprocess
 import sys
+import time
 
 from pathlib import Path
+
+from colorama import Fore, Style
 
 class TestFile:
 
     selected_functions : list[str] = []
+    functions_found : list[str] = []
+
     filepath : Path
     
     def __init__(self, filepath, selected_functions = []):
 
         self.filepath = filepath
+        self.functions_found = self.parse_test_function_names()
         self.selected_functions = self.load_selected_functions(selected_functions)
 
     def parse_test_function_names(self):
@@ -34,9 +40,7 @@ class TestFile:
 
         functions = []
 
-        test_function_names = self.parse_test_function_names()
-
-        for function_name in test_function_names:
+        for function_name in self.functions_found:
             if not selected_functions:
                 functions.append(function_name)
                 continue
@@ -64,7 +68,9 @@ class TestUnit:
             print(line)
     
     def __repr__(self) -> str:
-        return f'"{self.test_file.filepath}" {self.function_name} {"[Success]" if self.success else "[Failed]"}'
+
+        color = Fore.GREEN if self.success else Fore.RED
+        return f'"{self.test_file.filepath}" {self.function_name} {color} {"[PASSED]" if self.success else "[FAILED]"}{Fore.RESET}'
 
 class Collector:
 
@@ -78,7 +84,34 @@ class Collector:
             self.test_files.append(TestFile(path, selected_functions))
         if path.is_dir():
             self.test_files = [TestFile(path, selected_functions) for path in self.get_py_files_recursive(path)]
+    
+    def get_total_collected_tests(self) -> int:
+        
+        total = 0
 
+        for test in self.test_files:
+            total += len(test.functions_found)
+
+        return total
+    
+    def get_total_selected_tests(self) -> int:
+
+        total = 0
+
+        for test in self.test_files:
+            total += len(test.selected_functions)
+
+        return total
+    
+    def get_total_deselected_tests(self) -> int:
+
+        total = 0
+
+        for test in self.test_files:
+            total += len(test.functions_found) - len(test.selected_functions)
+
+        return total
+    
     def get_py_files_recursive(self, path):
 
         py_files = []
@@ -182,13 +215,18 @@ class BPYTEST_OT_Tests(bpy.types.Operator):
     
     _tests_list : list[TestUnit]
 
+    @staticmethod
+    def print_header(text : str, color, bold = True):
+        size = os.get_terminal_size()
+        print(color + (Style.BRIGHT if bold else "") +'{s:{c}^{n}}'.format(s= " " + text + " ",n=size.columns,c='=')+ Fore.RESET + Style.RESET_ALL)
+
     def _print_failed(self):
 
         for test in self._tests_list:
             if not test.success:
                 print("----------------------------------------------------------------------")
                 test.print_log()
-    
+
     def _compute_result(self):
 
         self._failed = 0
@@ -200,16 +238,22 @@ class BPYTEST_OT_Tests(bpy.types.Operator):
             if test.success:
                 self._success += 1
 
-        print("----------------------------------------------------------------------")
         for test in self._tests_list:
             if not test.success:
                 print(test)
-        print("----------------------------------------------------------------------")
-        print(f"Failed: {self._failed} Success: {self._success}")
-        print("----------------------------------------------------------------------")
+        
+        print("\n")
+
+        print_color = Fore.RED if self._failed else Fore.GREEN
+        center_text = f"Failed: {self._failed} Success: {self._success} in {self.total_time:.2f} seconds"
+
+        self.print_header(center_text, print_color)
+
 
     def execute(self, context):
 
+        self.print_header("Test session starts", Fore.WHITE)
+    
         if not self.source_directory:
             self.report({'ERROR'}, "Source Directory not set")
             return {'FINISHED'}
@@ -230,8 +274,19 @@ class BPYTEST_OT_Tests(bpy.types.Operator):
             self.report({'ERROR'}, "Test path not found: " + test_path.__str__())
             return {'FINISHED'}
         
-        print("Running tests from: " + test_path.__str__() + "\n")
+        print("rootdir: " + test_path.__str__())
         collector = Collector(test_path, config_file.selected_functions)
+        
+
+        if config_file.selected_functions:
+            print((f"{Style.BRIGHT}collected {str(collector.get_total_collected_tests())} items / "
+                   f"{str(collector.get_total_deselected_tests())} deselected / "
+                   f"{str(collector.get_total_selected_tests())} selected \n {Style.RESET_ALL}")
+                   )
+        else:
+            print(f"{Style.BRIGHT}collected {str(collector.get_total_collected_tests())} items \n {Style.RESET_ALL}")
+
+        self.start_time = time.time()
         
         for test_file in collector.test_files:
             for function_name in test_file.selected_functions:
@@ -244,6 +299,9 @@ class BPYTEST_OT_Tests(bpy.types.Operator):
 
                 print(test_process.test_unit)
                 self._tests_list.append(test_unit)
+        
+        self.end_time = time.time()
+        self.total_time = self.end_time - self.start_time
 
         self._print_failed()
         self._compute_result()
