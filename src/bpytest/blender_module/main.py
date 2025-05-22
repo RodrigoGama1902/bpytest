acc46f2a734ace6c3740c1ee149f20231bb6f38e
Rodrigo Gama <rodrigo1902.rb@gmail.com>
2025-05-22 20:29:33 -0300

    Added checks for already existing module names in the blender env

--- src/bpytest/blender_module/main.py
+++ src/bpytest/blender_module/main.py
@@ -1,10 +1,34 @@
 """This module is the entry point for the session blender subprocess."""
-
-import json
 import sys
 import traceback
 from pathlib import Path
 
+# ===================================================================================
+# Check to make sure that bpytest and bpytest_config modules are not already imported
+# ===================================================================================
+try:
+    import bpytest  # pylint: disable=unused-import
+    raise RuntimeError(
+        "bpytest module already exists in the sys.modules"
+        " and this is not allowed. Make sure that there is no 'bpytest'"
+        "installed directly in the blender python environment or as an addon."
+    )
+except ImportError:
+    pass
+
+try:
+    import bpytest_config  # pylint: disable=unused-import
+    raise RuntimeError(
+        "bpytest_config module already exists in the sys.modules"
+        " and this is not allowed. Make sure that there is no 'bpytest_config' "
+        "installed directly in the blender python environment or as an addon."
+    )
+except ImportError:
+    pass
+
+# ===================================================================================
+# Add the bpytest and bpytest_config modules to the sys.path
+# ===================================================================================
 # Append src/bpytest/common so any common module that is accessible
 # to the main entry point is also accessible inside the blender subprocess
 sys.path.append(Path(Path(__file__).parent.parent / "common").as_posix())
@@ -16,12 +40,15 @@ sys.path.append(Path(__file__).parent.as_posix())
 from bpytest import wrap_session  # pylint: disable=wrong-import-position
 
 
+# ===================================================================================
+# End of imports and sys.path modifications
+# ===================================================================================
 def main(config: BpyTestConfig, instance_id: str) -> int:
     """Main function"""
 
     for path in config.include:
         sys.path.append(path)
-        
+    
     sys.exit(wrap_session(config, instance_id))
 
 
@@ -34,12 +61,12 @@ try:
             data_json = arg[7:]
         if arg.startswith("instance_id"):
             instance_id = arg.split("=")[1]
-        
+
     if not data_json:
         raise ValueError("No config argument found")
     if not instance_id:
         raise ValueError("No instance_id argument found")
-    
+
     config = BpyTestConfig()
     config.load_from_json(data_json)
 
